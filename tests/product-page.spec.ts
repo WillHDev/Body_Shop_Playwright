da919a49e0ae7445d5e599bf5f04c2d276c4b5b6
WillHDev <will.v.holcombe@gmail.com>
2024-10-15 16:11:21 -0500

    second comit

---
+++ tests/product-page.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('Add product to cart', async ({ page }) => {
+  await page.goto('https://weathershopper.pythonanywhere.com/');
+  await page.getByText('Buy moisturizers').click();
+  await page
+  .locator(".text-center.col-4").nth(2)
+  .locator('text=Add')
+  .click(); 
+  await page
+  .locator(".text-center.col-4").nth(4)
+  .locator('text=Add')
+  .dblclick(); 
+  await page.locator('text=Cart - ').click();          
+  await page.getByRole('button', { name: 'Pay with Card' }).click();
+
+  await page.locator('.result-frame').contentFrame().getByRole('button').click();
+ // await page.getByPlaceholder('Email').fill('johngalt@gmail.com');
+ // await page.locator(`//button[@type='submit']//span[1]`).fill('johngalt@gmail.com');
+ await page.frameLocator('iframe[name="stripe_checkout_app"]').getByPlaceholder('Email').fill('johngalt@gmail.com');
+// await page.locator('input[type="email"]').fill('johngalt@gmail.com');
+  await page.getByPlaceholder('Card number').fill('12345678987654');
+  await page.getByPlaceholder('MM/YY').fill('08/28');
+  await page.getByPlaceholder('CVC').fill('434');
+  await page.getByRole('button', { name: /Pay INR/ }).click();
+});
+//
+
+//TODO
+
+//add sunscreen to cart
+//add moisturizer to cart
+//checkout
