2db85b313adb8ac6b22e95648b5b25509f14848a
WillHDev <will.v.holcombe@gmail.com>
2024-10-16 12:10:40 -0500

    checkout test completed and passing

--- tests/product-page.spec.ts
+++ tests/product-page.spec.ts
@@ -1,33 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('Add product to cart', async ({ page }) => {
-  await page.goto('https://weathershopper.pythonanywhere.com/');
-  await page.getByText('Buy moisturizers').click();
-  await page
-  .locator(".text-center.col-4").nth(2)
-  .locator('text=Add')
-  .click(); 
-  await page
-  .locator(".text-center.col-4").nth(4)
-  .locator('text=Add')
-  .dblclick(); 
-  await page.locator('text=Cart - ').click();          
-  await page.getByRole('button', { name: 'Pay with Card' }).click();
-
-  await page.locator('.result-frame').contentFrame().getByRole('button').click();
- // await page.getByPlaceholder('Email').fill('johngalt@gmail.com');
- // await page.locator(`//button[@type='submit']//span[1]`).fill('johngalt@gmail.com');
- await page.frameLocator('iframe[name="stripe_checkout_app"]').getByPlaceholder('Email').fill('johngalt@gmail.com');
-// await page.locator('input[type="email"]').fill('johngalt@gmail.com');
-  await page.getByPlaceholder('Card number').fill('12345678987654');
-  await page.getByPlaceholder('MM/YY').fill('08/28');
-  await page.getByPlaceholder('CVC').fill('434');
-  await page.getByRole('button', { name: /Pay INR/ }).click();
+test.describe('Cart', () => {
+  test('Add product to cart and checkout', async ({ page }) => {
+    await page.goto('https://weathershopper.pythonanywhere.com/');
+    await page.getByText('Buy moisturizers').click();
+    await page.locator(".text-center.col-4").nth(2)
+                .locator('text=Add')
+                  .click(); 
+    await page.locator(".text-center.col-4").nth(4)
+                  .locator('text=Add')
+                    .dblclick(); 
+    await page.locator('text=Cart - ').click();          
+    await page.getByRole('button', { name: 'Pay with Card' }).click();
+    const iframe = await page.frameLocator('iframe[name="stripe_checkout_app"]')
+    await iframe.getByPlaceholder('Email').fill('johngalt@gmail.com');;
+    await iframe.getByPlaceholder('Card number').fill('4242424242424242');
+    await iframe.getByPlaceholder('MM / YY').fill('08/28');
+    await iframe.getByPlaceholder('CVC').fill('434');
+    await iframe.getByRole('button', { name: /Pay/ }).click();
+    await expect(page.getByRole( 'heading', { name: "PAYMENT SUCCESS"})).toBeVisible();
+  });
 });
-//
-
-//TODO
-
-//add sunscreen to cart
-//add moisturizer to cart
-//checkout
